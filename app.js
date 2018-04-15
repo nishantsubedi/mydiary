18faa0f8ce13f1e37702de9ed133c26fc7211370
nishant <nishantsubedi1996@gmail.com>
2018-04-15 17:36:26 +0545

    routes, view engine, server, express-validator, express-messages, connect flash setup

--- app.js
+++ app.js
@@ -13,3 +13,60 @@ db.on('open', function() {
     console.log('Connected to mongodb');
 })
 
+// Init app
+const app = express();
+
+// View engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// set public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Body Parser middleware
+//
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+  //Express Validator middleware
+  app.use(expressValidator({
+    errorFormatter: (param, msg, value) => {
+        var namespace = param.split('.')
+        , root = namespace.shift()
+        , formParam = root;    
+        
+        while(namespace.length) {
+            formParam += '[' + namespace.shift()() + ']';
+        }
+        return {
+            param : formParam,
+            msg : msg,
+            value : value
+        };
+    }
+}));
+
+// Express Messages middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+// Set routes
+var home = require('./routes/home');
+var diary = require('./routes/diary');
+
+
+app.use('/', home);
+app.use('/diary', diary);
+
+
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+    console.log('Server started on port ' + port);
+});
+
