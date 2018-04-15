ad5915550e1e1a78f6c6db8a5b68573ab4d2a4b8
nishant <nishantsubedi1996@gmail.com>
2018-04-15 19:17:14 +0545

    created add a story view

--- routes/diary.js
+++ routes/diary.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
+// get diary
 router.get('/', (req, res) => {
     res.render('diary', {
         title: 'Diary'
     });
 });
+
+// GET write diaries page
+router.get('/write', (req, res) => {
+    res.render('diary/write', {
+        title: 'Write a chapter'
+    });
+});
+
 
 // Exports 
 module.exports = router;
