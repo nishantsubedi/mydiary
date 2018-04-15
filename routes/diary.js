8854f5289b5f5e1afe404ab0df9b6f179b955166
nishant <nishantsubedi1996@gmail.com>
2018-04-15 18:45:33 +0545

    views setup

--- routes/diary.js
+++ routes/diary.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('diary');
+    res.render('diary', {
+        title: 'Diary'
+    });
 });
 
 // Exports 
